32c25440bb2c227beae7a76166c7c0586cb30ec6
Abhinav Gupta <abhinavgupta@Abhinavs-MacBook-Pro.local>
2019-01-19 18:40:12 -0800

    deploy issue fix

---
+++ react-footer/precache-manifest.3ad9083d1c3e60e4799ace1d5c0ce888.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72ef6180544fe45641e8",
+    "url": "/react-footer/static/js/runtime~main.72ef6180.js"
+  },
+  {
+    "revision": "7e96dbf4874b9861a96f",
+    "url": "/react-footer/static/js/main.7e96dbf4.chunk.js"
+  },
+  {
+    "revision": "11dac3cdf202915bf55a",
+    "url": "/react-footer/static/js/1.11dac3cd.chunk.js"
+  },
+  {
+    "revision": "7e96dbf4874b9861a96f",
+    "url": "/react-footer/static/css/main.28c57131.chunk.css"
+  },
+  {
+    "revision": "f82d812b5d20042db04594f13bea469c",
+    "url": "/react-footer/index.html"
+  }
+];
--- react-footer/service-worker.js
+++ react-footer/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c23715effe6e57100764ca584ecc0f61.js"
+  "/react-footer/precache-manifest.3ad9083d1c3e60e4799ace1d5c0ce888.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-footer/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
